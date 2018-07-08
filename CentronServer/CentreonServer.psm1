cfbddfb912704b9d0af3c8e2b72528e28f00074e
ClissonFlorian <florian.clisson@gmail.com>
2018-07-08 13:34:11 +0200

    Update files location

---
+++ CentronServer/CentreonServer.psm1
@@ -0,0 +1,18 @@
+#Get public and private function definition files.
+$Public  = Get-ChildItem $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue 
+$Private = Get-ChildItem $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue 
+
+#Dot source the files
+foreach($import in @($Public + $Private)) {
+    try {
+        . $import.fullname
+    }
+    catch {
+        Write-Error "Failed to import function $($import.fullname)"
+    }
+}
+
+#Export public functions and the
+$PublicNames = $Public | Select -ExpandProperty BaseName
+
+Export-ModuleMember -Function $PublicNames
